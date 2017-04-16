60ed25795529b4f3a88826e0a6770c0fd9d4b81c
aminbys <aminbys@gmail.com>
2017-04-16 21:27:07 +0200

    Add direction correction for DSN networks

---
+++ Analysis/02_Perform_NOPs/CorrectGeneDirection.m
@@ -0,0 +1,10 @@
+function [xTr, xTe] = CorrectGeneDirection(xTr, xTe, lTr)
+n_gene = size(xTr, 2);
+gene_dir = corr(xTr, lTr);
+for gi=1:n_gene
+	if gene_dir(gi)<0
+		xTr(:, gi) = -xTr(:, gi);
+		xTe(:, gi) = -xTe(:, gi);
+	end
+end
+end
