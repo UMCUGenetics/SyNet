fc11484395541f2695f76e5141b40c0756519c77
Insy <aminbys@gmail.com>
2017-08-27 12:50:59 +0100

    Add evaluation of integration methods

---
+++ Analysis/14_Evaluate_Integration_Methods/S01_Generate_GeneSets.m
@@ -0,0 +1,38 @@
+clc;
+clear;
+
+%% Initialization
+addpath('../11_Perform_LassoTypes/');
+addpath('../../../../Useful_Sample_Codes/ShowProgress/');
+sav_path = 'NetNei_Files/';
+[~,~] = mkdir(sav_path);
+n_epoch = 10000;
+MAX_SUBNET_SIZE = 5;
+load('../../Gene_Expression_Datasets/SyNet/SyNet_BatchCorrected.mat', 'Gene_Name');
+n_gene = numel(Gene_Name);
+net_name = 'Random';
+
+%% Load network
+switch net_name
+    case 'Random'
+        Net_Adj = rand(n_gene);
+    otherwise
+        fprintf('Unknown method');
+end
+
+%% Generate Neighbor Sets
+fprintf('Generating neighbor sets and subnetworks: \n');
+Neig_cell = getNeighborsFromAdj(Net_Adj, MAX_SUBNET_SIZE);
+
+%% Generate Subnetworks
+SubNet_Full = cell(n_gene, 1);
+for gi=1:n_gene
+	showprogress(gi, n_gene);
+	SubNet_Full{gi} = getNetNeighborsBreadthFirst(Neig_cell, Neig_cell{gi}, MAX_SUBNET_SIZE, 1);
+end
+
+%% Save network neighbors
+sav_name = sprintf([sav_path 'NetNei_%s_NN%d.mat'], net_name, MAX_SUBNET_SIZE);
+fprintf('Saving network file to [%s]\n', sav_name);
+save(sav_name, 'SubNet_Full', 'Gene_Name');
+
