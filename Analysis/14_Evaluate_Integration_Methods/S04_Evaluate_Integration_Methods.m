2584f07f7bcb5e9b465ab7f72157fb2549bf97c9
Insy <aminbys@gmail.com>
2017-08-27 16:35:02 +0100

    Evaluate gene sets

--- Analysis/14_Evaluate_Integration_Methods/S04_Evaluate_Integration_Methods.m
+++ Analysis/14_Evaluate_Integration_Methods/S04_Evaluate_Integration_Methods.m
@@ -1,6 +1,17 @@
 function S04_Evaluate_Integration_Methods(Target_Study, Target_Repeat, method_name, net_name)
+%% Run
+%{
+for ri in `seq 1 5`; do
+for si in `seq 1 14`; do
+PARAM="$si,$ri,'PCA1','Random-NN05'"; sbatch --job-name=IE-$PARAM --output=Logs/NE-$PARAM.%J_%a-%N.out --partition=general --qos=short --mem=6GB --time=04:00:00 --ntasks=1 --cpus-per-task=1 run_Matlab.sh S04_Evaluate_Integration_Methods "$PARAM";
+done;
+done
+read -p "Press a key" -t 1800
+%}
+
 
 %% Initialization
+clc
 addpath('../../../../Useful_Sample_Codes/ShowProgress/');
 addpath('../../../../Useful_Sample_Codes/getAUC/');
 addpath('../_Utilities/');
@@ -9,8 +20,8 @@ tr_name = 'SyNet';
 te_name = 'SyNet';
 cv_ind = 50;
 if ispc
-    method_name = 'PCA1';
-    net_name = 'Random-NN05';
+    method_name = 'DPCA';
+    net_name = 'Random-NN20';
     Target_Study = 3;
     Target_Repeat = 2;
 end
@@ -28,6 +39,7 @@ if ~isequal(data.Gene_Name, cv_info.tr_info.Gene_Name), error(); end
 iTr = cv_info.tr_info.CVInd;
 zTr = zscore(data.Gene_Expression(iTr,:));
 lTr = (data.Patient_Label(iTr)==1)*2-1;
+n_gene = size(zTr, 2);
 
 %% Load Test data
 fprintf('Loading Test expression data from: %s\n', cv_info.te_info.GEPath);
@@ -46,19 +58,45 @@ if ~isequal(nei_info.Gene_Name, cv_info.tr_info.Gene_Name), error(); end
 n_epoch = numel(nei_info.SubNet_Full);
 nei_info.nei_name = nei_name;
 
+%% Get individual AUC
+resind_name = sprintf('./Result_Files/ResIND_%s-%s_CVT%02d_Si%02d-Ri%03d.mat', tr_name, te_name, cv_ind, Target_Study, Target_Repeat);
+if exist(resind_name, 'file')
+    fprintf('Individual AUCs are found in [%s]\n', resind_name);
+    load(resind_name, 'Gene_TrAUC');
+else
+    fprintf('Evaluating AUC of [%d] individual genes:\n', n_gene);
+    Gene_TrAUC = zeros(n_gene,1);
+    for gi=1:n_gene
+        showprogress(gi, n_gene);
+        Gene_TrAUC(gi,1) = getAUC(lTr, zTr(:,gi), 50);
+    end
+    save(resind_name, 'Gene_TrAUC');
+end
+
 %% Main loop
 fprintf('Comparing performance for [%d] neighbor sets:\n', n_epoch);
 Te_AUC = zeros(n_epoch,1);
+Ind_AUC = zeros(n_epoch,1);
 for ei=1:n_epoch
     showprogress(ei, n_epoch);
     nei_lst = nei_info.SubNet_Full{ei};
-    eTr = zTr(:, nei_lst); 
+    eTr = zTr(:, nei_lst);
     eTe = zTe(:, nei_lst);
+    grp_size = numel(nei_lst);
     
     switch method_name
         case 'PCA1'
             coeff = pca(eTr, 'NumComponents', 1);
             pred = eTe*coeff(:,1);
+        case 'DPCA'
+            coeff = pca(eTr);
+            pTr = eTr*coeff;
+            pc_auc = zeros(1, grp_size);
+            for pi=1:grp_size
+                pc_auc(pi) = getAUC(lTr, pTr(:,pi), 50);
+            end
+            [~, tp_ind] = max(pc_auc);
+            pred = eTe*coeff(:, tp_ind);
         otherwise
             error('Unknown method.');
     end
@@ -66,10 +104,12 @@ for ei=1:n_epoch
     %% Evaluate prediction
     Te_AUC(ei,1) = getAUC(lTe, pred, 50);
     %[~, ~, ~, auc] = perfcurve(lTe, pred, -1)
+    [~, tg_ind] = max(Gene_TrAUC(nei_lst));
+    Ind_AUC(ei) = getAUC(lTe, eTe(:,tg_ind), 50);
 end
 
 %% Saving results
 sav_name = sprintf('./Result_Files/RES_%s_%s_%s-%s_CVT%02d_Si%02d-Ri%03d.mat', method_name, net_name, tr_name, te_name, cv_ind, Target_Study, Target_Repeat);
 fprintf('Saving results in [%s]\n', sav_name);
-save(sav_name, 'Te_AUC', 'cv_info', 'nei_info');
+save(sav_name, 'Te_AUC', 'cv_info', 'nei_info', 'Ind_AUC', 'Gene_TrAUC');
 end
